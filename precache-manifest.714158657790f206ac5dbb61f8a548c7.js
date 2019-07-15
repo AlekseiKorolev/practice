477999bcd1654cf8a5c0ad380609ace738942bf6
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2019-07-15 05:33:43 +0200

    Updates

--- precache-manifest.714158657790f206ac5dbb61f8a548c7.js
+++ precache-manifest.714158657790f206ac5dbb61f8a548c7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/practice/static/js/runtime~main.abae3f0b.js"
   },
   {
-    "revision": "65b3d9ecee97f5363ab6",
-    "url": "/practice/static/js/main.65b3d9ec.chunk.js"
+    "revision": "63be57ebccc02dc7d695",
+    "url": "/practice/static/js/main.63be57eb.chunk.js"
   },
   {
     "revision": "d26cfaeb93e80be97c06",
     "url": "/practice/static/js/2.d26cfaeb.chunk.js"
   },
   {
-    "revision": "65b3d9ecee97f5363ab6",
+    "revision": "63be57ebccc02dc7d695",
     "url": "/practice/static/css/main.68e586d8.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/practice/static/css/2.107b0692.chunk.css"
   },
   {
-    "revision": "94047f471fcd52fdd1ab41b88323c0e0",
+    "revision": "7bdd13067823c4631deba40cd5ae2801",
     "url": "/practice/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/practice/precache-manifest.ffed0dcb3cc65d7ee3e9008c559be836.js"
+  "/practice/precache-manifest.714158657790f206ac5dbb61f8a548c7.js"
 );
 
 workbox.clientsClaim();
