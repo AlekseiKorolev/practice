a0aa68aa5cc02233a4da298c6f19b19c300907e4
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2019-09-17 11:14:57 +0200

    v1.04

--- src/panels/Search/Search.js
+++ src/panels/Search/Search.js
@@ -1,12 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Panel, PanelHeader, HeaderButton, Div, Button, Group, platform, IOS} from '@vkontakte/vkui';
+import {Panel, PanelHeader, HeaderButton, Div, Button, Group, platform, IOS, Select} from '@vkontakte/vkui';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
+import {searchData} from '../../util/requests';
 
 const osname = platform();
 
-const Search = props => (
+export class Search extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			side: '',
+			role: '',
+			format: '',
+			skill: '',
+			searchResult: 'Здесь отобразится результат поиска',
+		}
+
+		this.changeState = this.changeState.bind(this);
+		this.startSearch = this.startSearch.bind(this);
+		this.searchResult = this.searchResult.bind(this);
+		this.showSearchResult = this.showSearchResult.bind(this);
+	}
+
+	changeState(e){
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	startSearch(){
+		//console.log(this.state);
+		searchData(this.state, this.searchResult);
+	}
+
+	searchResult(e){
+		console.log(e);
+		if(e){
+			this.setState({ searchResult: JSON.parse(e)});
+		}else{
+			this.setState({ searchResult: 'Ничего не найдено'});
+		}
+	}
+
+	showSearchResult(){
+		//console.log(this.state.searchResult);
+		//console.log(this.state.searchResult);
+		if(Array.isArray(this.state.searchResult)){
+			return this.state.searchResult.map( id => {
+					if (Number(id.id_vk) !== this.props.userId.id) 
+					return <li key={id.id_vk}><a href={"https://vk.com/id" + id.id_vk} target="_blank">{id.first_name + ' ' + id.last_name}</a></li>
+				});
+		}else{
+			return <div>{this.state.searchResult}</div>
+		}
+	}
+
+	render(){
+		return(
+			<Panel id={this.props.id}>
+				<PanelHeader
+					left={<HeaderButton onClick={this.props.go} data-to="home">
+						{osname === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+					</HeaderButton>}
+				>
+					Поиск
+				</PanelHeader>
+				<br/>
+				<Div style={{ padding: '0px 2rem' }}>
+					<Select name="side" top="Предпочитаемая сторона" placeholder="любое" onChange={this.changeState}>
+						<option value="right">Право</option>
+						<option value="left">Лево</option>
+						<option value="center">Центр</option>
+					</Select>
+				</Div>
+				<br/>
+				<Div style={{ padding: '0px 2rem' }}>
+					<Select name="role" top="Амплуа" placeholder="любое" onChange={this.changeState}>
+						<option value="goalkeeper">Вратарь</option>
+						<option value="defender">Защитник</option>
+						<option value="semi-defender">Полузащитник</option>
+						<option value="forward">Нападающий</option>
+					</Select>
+				</Div>
+				<br/>
+				<Div style={{ padding: '0px 2rem' }}>
+					<Select name="format" top="Формат" placeholder="любое" onChange={this.changeState}>
+						<option value="11">11 x 11</option>
+						<option value="8">8 x 8</option>
+						<option value="6">6 x 6</option>
+					</Select>
+				</Div>
+				<br/>
+		    <Div style={{ padding: '0px 2rem' }}>
+		      <Select name="skill" top="Основное преимущество" placeholder="любое" onChange={this.changeState}>
+		        <option value="speed">Скорость</option>
+		        <option value="phisics">Физика</option>
+		        <option value="skill">Техника</option>
+		        <option value="pass">Пас</option>
+		        <option value="power">Удар</option>
+		      </Select>
+		    </Div>
+				<br/>
+				<Div>
+					<Button size="xl" level="1" onClick={this.startSearch}>
+						Искать
+					</Button>
+				</Div>
+				<Div>
+					<ul style={{listStyle: 'none'}}>
+						{this.showSearchResult()}
+					</ul>
+				</Div>
+			</Panel>
+		);
+	}
+}
+/*const Search = props => (
 	<Panel id={props.id}>
     <PanelHeader
       left={<HeaderButton onClick={props.go} data-to="home">
@@ -32,4 +141,4 @@ Search.propTypes = {
 	go: PropTypes.func.isRequired
 };
 
-export default Search;
+export default Search;*/
